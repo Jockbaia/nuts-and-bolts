13d0415c154856ae9f033b7d7e2529e3ff7f0843
pathselector-x <pathselector-x@github.com>
2022-04-24 03:16:04 +0200

    Added working camera and working grid-based movement

--- nuts&bolts/Assets/Script/BasicMovement.cs
+++ nuts&bolts/Assets/Script/BasicMovement.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-   
-    public Rigidbody rigidB;
-    public int speed = 10;
-    
+    public float moveSpeed = 5f;
+    public Transform movePoint;
 
-    void Start() {
-        rigidB = GetComponent<Rigidbody>();
+    void Start()
+    {
+        movePoint.parent = null;
     }
-    
+
     void Update()
     {
-         if(Input.GetKey("w")){ 
-            rigidB.AddForce(Vector3.forward * speed);
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) >= 0.1f && Mathf.Abs(Input.GetAxisRaw("Vertical")) >= 0.1f)
+            {
+                return;
+            }
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) >= 0.1f)
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) >= 0.1f)
+            {
+                movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
+            }
         }
-        
     }
 }
