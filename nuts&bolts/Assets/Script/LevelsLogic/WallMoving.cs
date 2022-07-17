067dfe3d702dd4ac8725cf4dfcd63e1443d4518d
pathselector-x <pathselector-x@github.com>
2022-07-17 20:35:00 +0200

    Done Tutorial3 level

---
+++ nuts&bolts/Assets/Script/LevelsLogic/WallMoving.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMoving : MonoBehaviour
+{
+    public Transform button;
+    bool doItOnce = true;
+
+    Vector3 initialPos;
+
+    void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (button.GetComponent<ButtonLogic>().isActive)
+        {
+            if (doItOnce)
+            {
+                doItOnce = false;
+                transform.Translate(new Vector3(0, -4, 0));
+            }
+        }
+        else
+        {
+            doItOnce = true;
+            transform.position = initialPos;
+        }
+    }
+}
---
+++ nuts&bolts/Assets/Script/LevelsLogic/WallMovingMulti.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovingMulti : MonoBehaviour
+{
+    public Transform button1;
+    public Transform button2;
+    bool doItOnce = true;
+
+    Vector3 initialPos;
+
+    void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (button1.GetComponent<ButtonLogic>().isActive && button2.GetComponent<ButtonLogic>().isActive)
+        {
+            if (doItOnce)
+            {
+                doItOnce = false;
+                transform.Translate(new Vector3(0, -4, 0));
+            }
+        }
+        else
+        {
+            doItOnce = true;
+            transform.position = initialPos;
+        }
+    }
+}
